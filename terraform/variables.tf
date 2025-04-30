b2b9ff1f301108c502aea79493bd09393f00c160
Akash Kumar <33365129+akashkr101@users.noreply.github.com>
2025-04-30 12:52:33 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,8 @@
-
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "key_name" {
+  description = "Name of the AWS EC2 Key Pair"
+}
